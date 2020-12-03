dba8779d19f4c8ea1fca5e5bd54c5fe2f0ef916a
sabali <sabadevs@gmail.com>
2020-12-03 12:58:17 +0300

    removed donut

--- desktopindex.php
+++ desktopindex.php
@@ -71,18 +71,7 @@
 .fa {
     font-size: 50px;
 }
-.donut{
-    margin-left: 750px;
-    margin-right: 100px;
-    position: absolute;
-    margin-top: 120px;
-    top: 0;
-      left: 0;
-      width: 120%;
-      height: 100%;
-      overflow: hidden;
-      z-index: 1;
-}
+
 .collap{
     margin-left: 750px;
     margin-right: 90px;
@@ -191,43 +180,8 @@
            
           </div>
 </div>
-<div class="donut">
-    
-    <!-- Donut Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                  <div class="chart-pie pt-4">
-                    <canvas id="myPieChart"></canvas>
-                  </div>
-                  <hr>
-                  Styling for the donut chart can be found in the <code>/js/demo/chart-pie-demo.js</code> file.
-                </div>
-              </div>
-            </div>
-          </div>
 
-        </div>
-</div>
-<div class="collap">
-    <div class="card shadow mb-4">
-                <!-- Card Header - Accordion -->
-                <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
-                  <h6 class="m-0 font-weight-bold text-primary">Collapsable Card Example</h6>
-                </a>
-                <!-- Card Content - Collapse -->
-                <div class="collapse show" id="collapseCardExample">
-                  <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse functionality. <strong>Click on the card header</strong> to see the card body collapse and expand!
-                  </div>
-                </div>
-              </div>
-</div>
+
 
 <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
